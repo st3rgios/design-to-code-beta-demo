32af735afdc262fc6e2254babc3e52701413e3e1
st3rgios <104406011+st3rgios@users.noreply.github.com>
2026-02-17 15:45:59 +0000

    test

---
+++ src/app/pages/waiting-list/waiting-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-waiting-list',
+  imports: [],
+  templateUrl: './waiting-list.html',
+  styles: ``,
+})
+export class WaitingList {
+
+}
